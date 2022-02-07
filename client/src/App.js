20eae2c7487868a8c8549ce824feeeafe903d132
Yazdani <yaz4dani@gmail.com>
2022-02-07 15:58:18 +0200

    new features

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,11 @@ function App() {
   const [alldata, setAlldata] = useState([]);
   const [singleuser, setSingleuser] = useState([]);
 
+  //input filed data
+
+  const [countresult, setCountresult] = useState("");
+  const [inputdata, setInputdata] = useState("");
+
   //to work with select option
 
   const loadData = () => {
@@ -30,18 +35,28 @@ function App() {
   }, []);
 
   const handleClick = (e) => {
-
-    fetch(singleurl+e.target.value, {
+    fetch(singleurl + e.target.value, {
       method: "GET",
     })
       .then((res) => res.json())
       .then((result) => {
-        setSingleuser(result);
-        console.log("Our Data from api is:" + result);
+        if (result) {
+          setSingleuser(result);
+          console.log("Our Data from api is:" + result);
+        }
       })
       .catch((err) => {
         console.log(err);
       });
+  };
+
+  const addsumTwonumbers = (e) => {
+    e.preventDefault();
+    var num1 =
+      (parseInt(singleuser.id) + parseInt(singleuser.id)) * parseInt(inputdata);
+    console.log("Addition result:" + num1);
+    setCountresult(num1);
+    setInputdata("");
   };
 
   return (
@@ -68,9 +83,29 @@ function App() {
         <p>Phone:{singleuser.phone}</p>
         <p>{singleuser.website}</p>
 
+        <p>{singleuser.id}</p>
 
-        <p>{JSON.stringify(singleuser)}</p>
+        <p>{parseInt(singleuser.id) * parseInt(singleuser.id)}</p>
 
+        <h1>Result is: {parseFloat(countresult).toFixed( 2 )}</h1>
+
+        {singleuser.id && <p>Now can show input field</p>}
+
+        {singleuser.id && (
+          <form>
+            <div>
+              <label>Add Change Date: {inputdata}</label>
+              <input
+                placeholder="add your number"
+                type="number"
+                step="0.1"
+                value={inputdata}
+                onChange={(e) => setInputdata(e.target.value)}
+              />
+            </div>
+            <button onClick={addsumTwonumbers}>Add</button>
+          </form>
+        )}
       </div>
     </React.Fragment>
   );
